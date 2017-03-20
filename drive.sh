0a9b0e3da25f8089eed42398af6d724b260cdaf6
Derek McQuay <dmcquay@apprenda.com>
2017-03-19 21:34:03 -0700

    add print outs to have idea where things are

--- drive.sh
+++ drive.sh
@@ -4,9 +4,12 @@ BIN=$ROOT/bin
 CNI=$ROOT/cni
 
 cd $BIN
+echo "building kubeadm"
 GOARCH=amd64 GOOS=linux go build $KUBEADM
 cd $ROOT
+echo "bringing up vms"
 vagrant up
+echo "copying over files"
 vagrant scp $BIN/kubeadm n1:~/
 vagrant scp $BIN/kubeadm n2:~/
 vagrant scp $CNI/kube-flannel.yml n1:~/
