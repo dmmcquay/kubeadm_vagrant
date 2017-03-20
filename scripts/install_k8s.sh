6b831623fb8e5f63ad581e1ae3aca1cfa68db84c
Derek McQuay <dmcquay@apprenda.com>
2017-03-19 21:34:19 -0700

    remove kubeadm since will push latest build

--- scripts/install_k8s.sh
+++ scripts/install_k8s.sh
@@ -3,4 +3,4 @@ set -e
 echo "Installing kubeadm prereqs..."
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update && sudo apt-get install -y docker-engine kubelet kubeadm kubectl kubernetes-cni
+sudo apt-get update && sudo apt-get install -y docker-engine kubelet kubectl kubernetes-cni
