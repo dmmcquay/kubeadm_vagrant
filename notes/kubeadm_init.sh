e4154b87ebb45482847414cd8d7c26669d65fecb
derek mcquay <derekmcquay@gmail.com>
2017-04-20 14:41:25 -0700

    add cmds for cni to notes

--- notes/kubeadm_init.sh
+++ notes/kubeadm_init.sh
@@ -1,2 +1,7 @@
 # command to run to get apiserver addr right and pod cidr based on flannel CNI
-sudo ~/kubeadm init --apiserver-advertise-address 172.42.42.1 --pod-network-cidr 10.244.0.0/16
+sudo ~/kubeadm init --apiserver-advertise-address 172.42.42.1 --pod-network-cidr 10.244.0.0/1
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+kubectl apply -f ~/kube-flannel-rbac.yml
+kubectl apply -f ~/kube-flannel.yml
