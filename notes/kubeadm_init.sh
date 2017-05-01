da469cff137785f6227423ee11225c7735144eab
Derek McQuay <derekmcquay@gmail.com>
2017-05-01 10:42:22 -0700

    Error in pod cidr
    Signed-off-by: Derek McQuay <derekmcquay@gmail.com>

--- notes/kubeadm_init.sh
+++ notes/kubeadm_init.sh
@@ -1,5 +1,5 @@
 # command to run to get apiserver addr right and pod cidr based on flannel CNI
-sudo ~/kubeadm init --apiserver-advertise-address 172.42.42.1 --pod-network-cidr 10.244.0.0/1
+sudo ~/kubeadm init --apiserver-advertise-address 172.42.42.1 --pod-network-cidr 10.244.0.0/16
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
